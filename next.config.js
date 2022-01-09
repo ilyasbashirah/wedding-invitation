e90e6f4e441536f08d738d4749ed6eeecd099bc0
milyasbpa <ilyas.bashirah@work.bri.co.id>
2022-01-09 23:34:11 +0700

    config: setup awal

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+// const { parsed: myEnv } = require('dotenv').config({
+//   path: `./deploy/environment/.env.${process.env.BRIBRAIN_ENV}`,
+// });
+module.exports = {
+  // dir: './src',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  assetPrefix: '/',
+  // async rewrites() {
+  //   return [
+  //     { source: '/:path*', destination: '/:path*' },
+  //     {
+  //       source: '/link5/:path*',
+  //       destination: `${process.env.MF_BRIBRAIN_LINK5}/:path*`,
+  //     },
+  //   ];
+  // },
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
